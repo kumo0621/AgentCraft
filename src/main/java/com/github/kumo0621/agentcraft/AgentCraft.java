4df5ca36f2ac9b147da2218c251c818a33cffbfe
kumo0621 <kumokumokumo0621@gmail.com>
2022-08-01 20:16:56 +0900

    switch文に変更

--- src/main/java/com/github/kumo0621/agentcraft/AgentCraft.java
+++ src/main/java/com/github/kumo0621/agentcraft/AgentCraft.java
@@ -4,15 +4,10 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.PluginCommand;
-import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.ArmorStand;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scoreboard.Team;
@@ -63,46 +58,33 @@ public final class AgentCraft extends JavaPlugin implements org.bukkit.event.Lis
                 String chat;
                 chat = e.getMessage();
                 System.out.println(chat);
-                String s1 = "上";
-                String s2 = "下";
-                String s3 = "前";
-                String s4 = "後ろ";
-                String s5 = "右";
-                String s6 = "左";
                 if (entity != null) {
                     Location loc = entity.getLocation();
-                    if (chat.equals(s1)) {
-                        loc.setY(loc.getY() + 1);
-                        player.sendMessage("上に移動しました。");
-                    } else if (chat.equals(s2)) {
-                        loc.setY(loc.getY() - 1);
-                        player.sendMessage("下に移動しました。");
-                    } else if (chat.equals(s3)) {
-                        loc.setX(loc.getX() + 1);
-                        player.sendMessage("前に移動しました。");
-                    } else if (chat.equals(s4)) {
-                        loc.setX(loc.getX() - 1);
-                        player.sendMessage("後ろに移動しました。");
-                    } else if (chat.equals(s5)) {
-                        loc.setZ(loc.getZ() + 1);
-                        player.sendMessage("右へ移動しました。");
-                    } else if (chat.equals(s6)) {
-                        loc.setZ(loc.getZ() - 1);
-
-                        player.sendMessage("左へ移動しました。");
-
+                    switch (chat) {
+                        case "上":
+                            loc.setY(loc.getY() + 1);
+                            break;
+                        case "下":
+                            loc.setY(loc.getY() - 1);
+                            break;
+                        case "前":
+                            loc.setX(loc.getX() + 1);
+                            break;
+                        case "後ろ":
+                            loc.setX(loc.getX() - 1);
+                            break;
+                        case "右":
+                            loc.setZ(loc.getZ() + 1);
+                            break;
+                        case "左":
+                            loc.setZ(loc.getZ() - 1);
+                            break;
                     }
                     entity.teleport(loc);
                 }
-
             }
         });
     }
-
-
-    //entity に同じことをする。
-    //playerからteamを取得してteamからアーマースタンドを取得する。
-    //でいける！！
 
 
     boolean tick;
@@ -129,13 +111,11 @@ public final class AgentCraft extends JavaPlugin implements org.bukkit.event.Lis
                             @NotNull ArmorStand entity = location.getWorld().spawn(location, ArmorStand.class);
                             entity.setGravity(false);
                             map.put(team, entity);
-                        }
-//チームがないときのメッセージ宿題
-                        //mapがからの時条件
+                            sender.sendMessage("アーマースタンドを召喚しました。");
+                        } else sender.sendMessage("チームが存在しません");
 
                     }
 
-                    sender.sendMessage("アーマースタンドを召喚しました。");
                 } else if (args[0].equals("start")) {
                     tick = true;
                     System.out.println(tick);
